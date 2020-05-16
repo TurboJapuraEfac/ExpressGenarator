fe685391bd718f39acb31b7d8b9c235e6574b393
TurboJapuraEfac <srie61509@gmail.com>
2020-05-16 17:13:19 +0530

    File Upload

---
+++ routes/uploadRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const authenticate = require("../authenticate");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+
+  filename: (req, file, cb) => {
+      //file name how it is going to be stored = originalname
+      // this uses client side name 
+      //if not random string multer will give 
+    cb(null, file.originalname);
+  },
+});
+
+//kind of files that you can upload
+const imageFileFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error("You can upload only image files!"), false);
+  }
+  cb(null, true);
+};
+
+//udea declare karpuwa multur ekata denawa
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(bodyParser.json());
+
+uploadRouter
+  .route("/")
+  .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;                    
+    res.end("GET operation not supported on /imageUpload");
+  })
+
+  //meke next naaa                                                      //key eka postman wala use karna
+.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    //this contains the path of the image and passback to the client
+    res.json(req.file);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /imageUpload');
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /imageUpload');
+});
+
+module.exports = uploadRouter;
